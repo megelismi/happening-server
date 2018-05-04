d64397f40bb041cdcc01971be6dc46c3219b909a
Megan Smith <meg.eli.smi@gmail.com>
2018-05-04 16:21:04 -0700

    fandango

--- handlers/fandango.js
+++ handlers/fandango.js
@@ -3,8 +3,41 @@ import fetch from 'node-fetch';
 require('dotenv').config();
 
 const apiKey = process.env.FANDANGO_API_KEY;
+const sharedSecret = process.env.FANDANGO_SHARED_SECRET;
 
 console.log('apiKey', apiKey);
+
+var http = require('http');
+
+String.prototype.format = String.prototype.f = function() {
+
+    var s = this,
+    i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+}
+
+function sha256Encode(stringToEncode) {
+
+    var crypto = require('crypto');
+    var result = crypto.createHash('sha256').update(stringToEncode).digest('hex');
+
+    return result;
+}
+
+function buildAuthorizationParameters(apiKey, sharedSecret) {
+
+    var seconds = Math.floor(new Date() / 1000);
+    var paramsToEncode = apiKey + sharedSecret + seconds;
+    var encodedParams = sha256Encode(paramsToEncode);
+
+    var result = 'apikey={0}&sig={1}'.format(apiKey, encodedParams);
+
+    return result;
+}
 
 class Fandango {
 
@@ -26,21 +59,33 @@ class Fandango {
     */
 
     getMovieInfo(callback) {
-        fetch(this._urlPath())
-            .then(response => {
-                console.log('response', response.json());
+         var baseUri = 'http://api.fandango.com';
+        var apiVersion = '1';
 
-                //response.json();
-            }).then(json => {
-                    //console.log('got a json response', json);
+        var zipCode = '95811';
+        var parameters = 'op=theatersbypostalcodesearch&postalcode={0}'.format(zipCode);
 
-                    //callback(null, json);
-            }).catch(error => {
-                    console.log('got an error', error);
+        // Use your account-specific values here
 
-                    //callback(error);
-            })
-        ;
+        var authorizationParameters = buildAuthorizationParameters(apiKey, sharedSecret);
+
+        var requestUri = '{0}/v{1}/?{2}&{3}'.format(baseUri, apiVersion, parameters, authorizationParameters);
+
+        var response = '';
+
+        http.get(requestUri, function(apiRes) {
+                 apiRes.on('data', function(data) {
+                        response += data;
+
+                        console.log('RESPONSE', response);
+                    });
+
+                 apiRes.on('end', function() {
+                           //serverRes.end(response);
+                    });
+                 });
+
+        return 'done';
     }
 
     /**
